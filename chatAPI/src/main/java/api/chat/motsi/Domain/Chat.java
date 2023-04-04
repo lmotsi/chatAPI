2cc56246ba7df7629e56439da7f20094618e9d2c
lmotsi021 <lmotsi021@gmail.com>
2023-04-04 14:25:37 +0200

    User can now send a message

--- chatAPI/src/main/java/api/chat/motsi/Domain/Chat.java
+++ chatAPI/src/main/java/api/chat/motsi/Domain/Chat.java
@@ -1,14 +1,20 @@
 package api.chat.motsi.Domain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Chat {
     public static int numberOfChats;
     private ArrayList<Message> messages;
     private int chatID;
+    private ArrayList<User> users;
 
-    public Chat() {
+    public Chat(User user1, User user2) {
+        this.users = new ArrayList<>();
+        this.users.add(user1);
+        this.users.add(user2);
         this.messages = new ArrayList<>();
+
     }
 
     public Chat generateID() {
@@ -21,4 +27,17 @@ public class Chat {
         this.messages.add(message);
     }
 
+    public boolean hasUsers(User a, User b) {
+        return has(a) && has(b);
+
+    }
+
+    private boolean has(User user) {
+        for (User inChat : this.users) {
+            if (user.equals(inChat)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- chatAPI/src/main/java/api/chat/motsi/Domain/User.java
+++ chatAPI/src/main/java/api/chat/motsi/Domain/User.java
@@ -1,17 +1,31 @@
 package api.chat.motsi.Domain;
 
+import java.util.ArrayList;
+
 public class User {
 
     private String userName;
     private String passWord;
 
+    private ArrayList<Chat> chats;
+
     public User(String name, String passWord) {
         this.userName = name;
         this.passWord = passWord;
+        this.chats = new ArrayList<>();
     }
 
     public String getUserName() {
         return this.userName;
+    }
+
+    public void sendMessage(String text, User receiver) {
+        Message message = new Message(text, this, receiver);
+        for (Chat chat: this.chats) {
+            if (chat.hasUsers(this, receiver)) {
+                chat.addMessage(message);
+            }
+        }
     }
 
     public Boolean confirmPassWord(String enteredPassword) {
