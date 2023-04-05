3ac33b4ae2d29da7215587621603284d5b5f9f29
Lesego Motsi <lsgmotsi@gmail.com>
2023-04-06 01:23:17 +0200

    I can now connect to the database which is only in my machine, windows specifically, need to make it be accessed by anyone who wants to play with the code

--- chatAPI/src/main/java/api/chat/motsi/Persistence/Driver.java
+++ chatAPI/src/main/java/api/chat/motsi/Persistence/Driver.java
@@ -1,18 +1,21 @@
 package api.chat.motsi.Persistence;
 
+import java.io.*;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Driver {
 
     Connection connection;
-    private Driver() throws SQLException {
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatAPIdb");
+    private Driver(String password) throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatAPIdb","root", password);
     }
 
     public static void main(String[] args) {
 
         try {
-            Driver driver = new Driver();
+            System.out.print("Please enter the password to connect to the SQL server: ");
+            Driver driver = new Driver(new Scanner(System.in).nextLine());
             System.out.println("Success!!!");
         } catch (SQLException e) {
             throw new RuntimeException(e);
